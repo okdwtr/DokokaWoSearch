de6229c418c6ed8b53c3ece2cff2b624f0abf8cd
OKD <40514744+okdwtr@users.noreply.github.com>
2025-07-05 14:25:27 +0900

    fix typo

--- content.js
+++ content.js
@@ -4,8 +4,8 @@ window.addEventListener('load', () => {
     const destinationList = document.querySelector('.destination_list');
     if (destinationList) {
       const textContent = destinationList.textContent;
-      const allTermsHavving = searchTerms.every(term => new RegExp(term).test(textContent));
-      if (!allTermsHavving) {
+      const allTermsHaving = searchTerms.every(term => new RegExp(term).test(textContent));
+      if (!allTermsHaving) {
         const searchButton = document.getElementById('link_search');
         if (searchButton) {
           searchButton.click();
