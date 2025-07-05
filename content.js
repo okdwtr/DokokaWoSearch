101ea060bd4ec7fcef3ce0d672b68cc9ab92cb6e
OKD <40514744+okdwtr@users.noreply.github.com>
2025-07-05 14:33:09 +0900

    add console log

--- content.js
+++ content.js
@@ -4,11 +4,24 @@ window.addEventListener('load', () => {
     const destinationList = document.querySelector('.destination_list');
     if (destinationList) {
       const textContent = destinationList.textContent;
+      const results = Array.from(document.querySelectorAll('.destination_list h3.ttl a')).map(a => a.textContent);
       const allTermsHavving = searchTerms.every(term => new RegExp(term).test(textContent));
       if (!allTermsHavving) {
+        console.info("Not all search terms are present.\n", results.join(", "));
         const searchButton = document.getElementById('link_search');
         if (searchButton) {
           searchButton.click();
+        }
+      }
+      else {
+        console.info("All search terms are present.\n", results.join(", "));
+      }
+    }
+    else {
+      const err = document.querySelector('.mainttl');
+      if (err) {
+        if (err.textContent == "検索回数上限エラー") {
+          console.log("Error occurred in the search limit.");
         }
       }
     }
